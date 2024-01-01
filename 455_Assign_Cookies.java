bdf284b7c3b96e96672d8ef349b28643826d7c2a
Aryan Chouhan <136920253+aryanc17@users.noreply.github.com>
2024-01-01 10:39:43 +0530

    First commit

---
+++ 455_Assign_Cookies.java
@@ -0,0 +1,23 @@
+//Leetcode 455. Assign Cookies
+
+import java.util.*;
+
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int cnt = 0;
+        int i=0, j= 0;
+
+        while(i < g.length && j < s.length){
+            if(g[i] <= s[j]){
+                cnt++;
+                i++;
+                j++;
+            }
+            else j++;
+        }
+        return cnt;
+    }
+}
