c7f462150b56bab6b304bac32c942a6dd8035b6a
Aryan Chouhan <136920253+aryanc17@users.noreply.github.com>
2024-01-14 10:40:18 +0530

    1657. Determine if Two Strings Are Close

---
+++ 1657_Leetcode.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public boolean closeStrings(String word1, String word2) {
+        int[] map1 = new int[26];
+        int[] map2 = new int[26];
+
+        for(char c : word1.toCharArray()) map1[c - 'a']++;
+        for(char c : word2.toCharArray()) map2[c - 'a']++;
+
+        for(int i=0; i<26; i++){
+            if(map1[i] == 0 && map2[i] != 0) return false;
+            if(map1[i] != 0 && map2[i] == 0) return false;
+        }
+
+        Arrays.sort(map1);
+        Arrays.sort(map2);
+
+        return Arrays.equals(map1, map2);
+    }
+}
