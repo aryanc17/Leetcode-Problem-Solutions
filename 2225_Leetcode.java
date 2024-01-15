137b65527b6cfe4e4a3bc75bfbc7b1c482b50485
Aryan Chouhan <136920253+aryanc17@users.noreply.github.com>
2024-01-15 10:54:45 +0530

    2225. Find Players With Zero or One Losses

---
+++ 2225_Leetcode.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> findWinners(int[][] matches) {
+        List<Integer> zeroLoses = new ArrayList<>();
+        List<Integer> oneLoses = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int team[] : matches){
+            map.put(team[0], map.getOrDefault(team[0] , 0) + 0);
+            map.put(team[1], map.getOrDefault(team[1] , 0) + 1);
+        }
+
+        for(int i : map.keySet()){
+            if(map.get(i) == 0){
+                zeroLoses.add(i);
+            }else if(map.get(i) == 1){
+                oneLoses.add(i);
+            }
+        }
+
+        Collections.sort(zeroLoses);
+        Collections.sort(oneLoses);
+
+        return Arrays.asList(zeroLoses, oneLoses);
+    }
+}
